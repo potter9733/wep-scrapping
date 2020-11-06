45a1478f7ed44bcaa0553fef804dfadba2b045ea
potter9733 <potter9733@users.noreply.github.com>
2020-11-06 15:28:14 +0900

    about_class

--- indeed.py
+++ indeed.py
@@ -49,6 +49,6 @@ def extract_jobs(last_page):
 def get_jobs():
     last_page = get_last_pages()
 
-    jobs = extract_jobs(last_page)
+    jobs = extract_jobs(1)
 
     return jobs
--- stackoverflow.py
+++ stackoverflow.py
@@ -38,5 +38,5 @@ def extract_jobs(last_page):
     
 def get_jobs():
     last_page = get_last_page()
-    jobs = extract_jobs(last_page)
+    jobs = extract_jobs(1)
     return jobs
